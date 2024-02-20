5b8142c988a53ccfaff44ad874034fc602cd80ad
prakassh <prakassh@prakassh>
2024-02-20 17:36:06 +0530

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Kannimara
+
+Family Restaurant
+
+#### License
+
+MIT
---
+++ kannimara/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Kannimara",
+			"type": "module",
+			"label": _("Kannimara")
+		}
+	]
